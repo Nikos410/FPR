9007cd300107c56ae92426900081c54139cc7055
Nikos Epping <nikos.epping@web.de>
2020-11-23 17:59:04 +0100

    MatrixRowTest: Use varargs parameter

--- gauss/src/test/java/de/nikos410/fpr/gauss/MatrixRowTest.java
+++ gauss/src/test/java/de/nikos410/fpr/gauss/MatrixRowTest.java
@@ -8,15 +8,13 @@ public class MatrixRowTest {
 
     @Test
     public void testToString() {
-        final Double[] values = {47.11, 410d, 42d};
-        final MatrixRow testRow = new MatrixRow(values);
+        final MatrixRow testRow = new MatrixRow(47.11, 410d, 42d);
         assertThat(testRow.toString()).isEqualTo("47.11 | 410.0 | 42.0");
     }
 
     @Test
     public void testToStringWithNull() {
-        final Double[] values = {47.11, null, 42d};
-        final MatrixRow testRow = new MatrixRow(values);
+        final MatrixRow testRow = new MatrixRow(47.11, null, 42d);
         assertThat(testRow.toString()).isEqualTo("47.11 | null | 42.0");
     }
 }
