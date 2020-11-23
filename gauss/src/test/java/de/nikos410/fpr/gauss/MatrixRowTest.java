a66f9d27720de6cf965da4466ea99edf7a23a932
Nikos Epping <nikos.epping@web.de>
2020-11-23 17:51:02 +0100

    MatrixRowTest: Check for correct behaviour for null values

--- gauss/src/test/java/de/nikos410/fpr/gauss/MatrixRowTest.java
+++ gauss/src/test/java/de/nikos410/fpr/gauss/MatrixRowTest.java
@@ -14,4 +14,11 @@ public class MatrixRowTest {
         final MatrixRow testRow = new MatrixRow(Arrays.asList(values));
         assertThat(testRow.toString()).isEqualTo("47.11 | 410.0 | 42.0");
     }
+
+    @Test
+    public void testToStringWithNull() {
+        final Double[] values = {47.11, null, 42d};
+        final MatrixRow testRow = new MatrixRow(Arrays.asList(values));
+        assertThat(testRow.toString()).isEqualTo("47.11 | null | 42.0");
+    }
 }
