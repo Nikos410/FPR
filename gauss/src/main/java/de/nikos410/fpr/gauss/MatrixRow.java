e359404fc74d0b4e5f1ec1d35bf7bef08c2e9711
Nikos Epping <nikos.epping@web.de>
2020-11-23 17:54:10 +0100

    MatrixRow: Allow creating instance from array

--- gauss/src/main/java/de/nikos410/fpr/gauss/MatrixRow.java
+++ gauss/src/main/java/de/nikos410/fpr/gauss/MatrixRow.java
@@ -1,11 +1,16 @@
 package de.nikos410.fpr.gauss;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class MatrixRow {
     private final List<Double> values;
+
+    public MatrixRow(Double[] values) {
+        this(Arrays.asList(values));
+    }
 
     public MatrixRow(List<Double> values) {
         this.values = values;
--- gauss/src/test/java/de/nikos410/fpr/gauss/MatrixRowTest.java
+++ gauss/src/test/java/de/nikos410/fpr/gauss/MatrixRowTest.java
@@ -2,8 +2,6 @@ package de.nikos410.fpr.gauss;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixRowTest {
@@ -11,14 +9,14 @@ public class MatrixRowTest {
     @Test
     public void testToString() {
         final Double[] values = {47.11, 410d, 42d};
-        final MatrixRow testRow = new MatrixRow(Arrays.asList(values));
+        final MatrixRow testRow = new MatrixRow(values);
         assertThat(testRow.toString()).isEqualTo("47.11 | 410.0 | 42.0");
     }
 
     @Test
     public void testToStringWithNull() {
         final Double[] values = {47.11, null, 42d};
-        final MatrixRow testRow = new MatrixRow(Arrays.asList(values));
+        final MatrixRow testRow = new MatrixRow(values);
         assertThat(testRow.toString()).isEqualTo("47.11 | null | 42.0");
     }
 }
