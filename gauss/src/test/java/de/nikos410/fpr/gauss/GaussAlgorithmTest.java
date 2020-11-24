e05d8b154969836015ed18eafffeb4558f57af24
Nikos Epping <nikos.epping@web.de>
2020-11-24 23:33:05 +0100

    GaussAlgorithmTest: Update test to check pivoting

--- gauss/src/test/java/de/nikos410/fpr/gauss/GaussAlgorithmTest.java
+++ gauss/src/test/java/de/nikos410/fpr/gauss/GaussAlgorithmTest.java
@@ -3,30 +3,30 @@ package de.nikos410.fpr.gauss;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GaussAlgorithmTest {
 
     @Test
     public void testElimination() {
-        final GaussAlgorithm gaussAlgorithm = new GaussAlgorithm(createInputMatrix());
+        final GaussAlgorithm gaussAlgorithm = new GaussAlgorithm(createInputMAtrix());
         Assertions.assertThat(gaussAlgorithm.eliminate()).isEqualTo(createExpectedResultMatrix());
     }
 
-    private static Matrix createInputMatrix() {
-        final List<MatrixRow> inputRows = new LinkedList<>();
-        inputRows.add(new MatrixRow(2d, -1d, 3d, 1d));
-        inputRows.add(new MatrixRow(3d, 1d, -2d, 0d));
-        inputRows.add(new MatrixRow(1d, 1d, 1d, 3d));
+    private static Matrix createInputMAtrix() {
+        final List<MatrixRow> inputRows = new ArrayList<>();
+        inputRows.add(new MatrixRow(0d, 2d, 3d, 4d));
+        inputRows.add(new MatrixRow(1d, 1d, 1d, 2d));
+        inputRows.add(new MatrixRow(3d, 3d, 1d, 0d));
         return new Matrix(inputRows);
     }
 
     private static Matrix createExpectedResultMatrix() {
-        final List<MatrixRow> inputRows = new LinkedList<>();
-        inputRows.add(new MatrixRow(2d, -1d, 3d, 1d));
-        inputRows.add(new MatrixRow(0d, 2.5d, -6.5d, -1.5d));
-        inputRows.add(new MatrixRow(0d, 0d, 3.4d, 3.4d));
+        final List<MatrixRow> inputRows = new ArrayList<>();
+        inputRows.add(new MatrixRow(3d, 3d, 1d, 0d));
+        inputRows.add(new MatrixRow(0d, 2d, 3d, 4d));
+        inputRows.add(new MatrixRow(0d, 0d, 0.6666666666666667, 2d));
         return new Matrix(inputRows);
     }
 }
