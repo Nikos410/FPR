0c66e6d61640a85f466c62a43bd510f3b4743de2
Nikos Epping <nikos.epping@web.de>
2020-11-24 16:38:55 +0100

    GaussAlgorithmCommandLineRunner: Add some informative output on how to enter the matrix rows

--- gauss/src/main/java/de/nikos410/fpr/gauss/GaussAlgorithmCommandLineRunner.java
+++ gauss/src/main/java/de/nikos410/fpr/gauss/GaussAlgorithmCommandLineRunner.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GaussAlgorithmCommandLineRunner {
     public static void main(String[] args) {
@@ -17,12 +18,15 @@ public class GaussAlgorithmCommandLineRunner {
     }
 
     private static Matrix readInputMatrix() {
+        System.out.println("Please enter the matrix rows, or press enter again if you are done.");
+        System.out.println("Please enter the complete matrix rows, separated by spaces. For example, the formula '2x +3y -4z = 2' should be entered as '2 3 -4 2'");
+
         final List<MatrixRow> rows = new LinkedList<>();
 
         try (final InputStreamReader inputStreamReader = new InputStreamReader(System.in);
              final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
             while (true) {
-                System.out.print("Enter a row, or press enter if you are done.\n> ");
+                System.out.print("\n> ");
 
                 final String input = bufferedReader.readLine();
                 if (input.isBlank()) {
@@ -30,6 +34,7 @@ public class GaussAlgorithmCommandLineRunner {
                 }
 
                 rows.add(new MatrixRowParser(input).parse());
+                System.out.println(rows.stream().map(MatrixRow::toString).collect(Collectors.joining("\n")));
             }
         } catch (IOException e) {
             throw new IllegalStateException(e);
