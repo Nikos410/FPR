3569c274afea4f7c72f82f91cea539b6211f6c9f
Nikos Epping <nikos.epping@web.de>
2020-12-16 12:58:39 +0100

    employee-management: Add AbstractPerson

---
+++ employee-management/src/main/java/de/nikos410/fpr/employeemanagement/personal/AbstractPerson.java
@@ -0,0 +1,54 @@
+package de.nikos410.fpr.employeemanagement.personal;
+
+import java.util.Objects;
+
+public abstract class AbstractPerson {
+    private String firstName;
+    private String lastName;
+
+    public AbstractPerson(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AbstractPerson that = (AbstractPerson) o;
+        return firstName.equals(that.firstName) && lastName.equals(that.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractPerson{" +
+                "firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                '}';
+    }
+}
