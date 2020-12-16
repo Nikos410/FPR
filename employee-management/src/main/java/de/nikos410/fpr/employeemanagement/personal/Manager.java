efa9a4a38842d9c1efcf99ebe549f36786e592e2
Nikos Epping <nikos.epping@web.de>
2020-12-16 12:58:48 +0100

    employee-management: Add Manager

---
+++ employee-management/src/main/java/de/nikos410/fpr/employeemanagement/personal/Manager.java
@@ -0,0 +1,54 @@
+package de.nikos410.fpr.employeemanagement.personal;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Manager extends Employee {
+    private BigDecimal bonus;
+
+    public Manager(String firstName, String lastName, BigDecimal salary, LocalDate hireDate, long staffId, BigDecimal bonus) {
+        super(firstName, lastName, salary, hireDate, staffId);
+        this.bonus = bonus;
+    }
+
+    public BigDecimal getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(BigDecimal bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        Manager manager = (Manager) o;
+        return bonus.equals(manager.bonus);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), bonus);
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", salary=" + getSalary() +
+                ", hireDate=" + getHireDate() +
+                ", staffId=" + getStaffId() +
+                ", bonus=" + getBonus() +
+                '}';
+    }
+}
